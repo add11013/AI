381b12677e14ff5d847a26f2f33881cbe9731c10
Roderick <j8888888871@yahoo.com.tw>
2018-06-19 03:33:43 +0800

    2018

--- Master Dissertation/Result/ABCO_Result/PaperStrategy.m
+++ Master Dissertation/Result/ABCO_Result/PaperStrategy.m
@@ -1,52 +1,27 @@
-function [output_ProfitTable]=PaperStrategy(LoadName,Target,NumberOfTrial)
+function [Profit,SellAndBuy]=PaperStrategy(Actual,Forecast)
 output_ProfitTable=[];
 tempProfitTable=[];
-for iiii=1:NumberOfTrial
-    OriginalData=[];
-    load([LoadName int2str(iiii)])
-    for iii=1:300
-        Alpha=iii*0.001-0.001
-        
-        for N=1:NumberOfOUTPUT
-            FinalyHead(:,N)=[ABCOgBest.yHead(:,N) ;testyHead(:,N)];
-        end
-        TMP=[];
-        j1=1;
-        for N=1:NumberOfOUTPUT
-            TMP=OriginalData(31:length(OriginalData)-1,j1)+real(FinalyHead(:,N));
-            PredictClose(:,j1)=TMP(1:NumberOfAllPoint-1);
-            TMP=OriginalData(31:length(OriginalData)-1,j1+1)+imag(FinalyHead(:,N));
-            PredictClose(:,j1+1)=TMP(1:NumberOfAllPoint-1);
-            j1=j1+2;
-        end
-        
-        
-        
-        actual=OriginalData(33:length(OriginalData),Target);
-        buy=0;
-        sale=0;
-        SaleMoney=0;
-        BuyMoney=0;
-        Forecast=PredictClose(:,Target);
-        for i=1:length(Forecast)-1
-            %sell
-            if abs(Forecast(i)-actual(i))/actual(i)<=Alpha && (Forecast(i+1)-actual(i))>0
-                sale=sale+1;
-                SaleMoney=SaleMoney+actual(i+1)-actual(i);
-            end
-            
-            if abs(Forecast(i)-actual(i))/actual(i)<=Alpha && (Forecast(i+1)-actual(i))<0
-                buy=buy+1;
-                BuyMoney=BuyMoney+actual(i)-actual(i+1);
-            end
-        end
-        Profit=SaleMoney+BuyMoney;
-        ProfitTable(1)=Profit;
-        ProfitTable(2)=sale;
-        ProfitTable(3)=buy;
-        tempProfitTable=[tempProfitTable;ProfitTable];
-        output_ProfitTable(iii).value(iiii,:)=ProfitTable;
-    end
-end
 
+for iii=1:300
+    Alpha=iii*0.001-0.001;
+    buy=0;
+    sell=0;
+    SaleMoney=0;
+    BuyMoney=0;
+    for i=1:length(Forecast)-1
+        %sell
+        if abs(Forecast(i)-Actual(i))/Actual(i)<=Alpha && (Forecast(i+1)-Actual(i))>0
+            sell=sell+1;
+            SaleMoney=SaleMoney+Actual(i+1)-Actual(i);
+        end
+        %buy
+        if abs(Forecast(i)-Actual(i))/Actual(i)<=Alpha && (Forecast(i+1)-Actual(i))<0
+            buy=buy+1;
+            BuyMoney=BuyMoney+Actual(i)-Actual(i+1);
+        end
+    end
+    Profit(iii,1)=SaleMoney+BuyMoney;
+    SellAndBuy(iii,1)=sell;
+    SellAndBuy(iii,2)=buy;
+end
 end
--- Master Dissertation/Result/ABCO_Result/main.m
+++ Master Dissertation/Result/ABCO_Result/main.m
@@ -1,18 +1,84 @@
 clear
 clc
- 
-Target=1;
+
+NumberOfProfitTarget=1;
 NumberOfTrial=10;
-LoadName='ABCOResult_EX3_trial';
-LoadName1='1ABCOResult_Ex3_trial';
-ProfitTable=PaperStrategy(LoadName,Target,NumberOfTrial);
-ProfitTable1=PaperStrategy(LoadName1,Target,NumberOfTrial);
-for i=1:300
-    Mean(i)=mean(ProfitTable(i).value(:,1));
-    Mean1(i)=mean(ProfitTable1(i).value(:,1));
+LoadName='ABCOResult_EX1_trial';
+
+%% 100 iterations
+ProfitTable=0;
+for Target=1:NumberOfProfitTarget
+    for Trial=1:NumberOfTrial
+        load([LoadName int2str(Trial)])
+        for N=1:NumberOfOUTPUT
+            FinalyHead(:,N)=[ABCOgBest.yHead(:,N) ;testyHead(:,N)];
+        end
+        TMP=[];
+        j1=1;
+        for N=1:NumberOfOUTPUT
+            TMP=OriginalData(31:length(OriginalData)-1,j1)+real(FinalyHead(:,N));
+            PredictClose(:,j1)=TMP(1:NumberOfAllPoint-1);
+%             TMP=OriginalData(31:length(OriginalData)-1,j1+1)+imag(FinalyHead(:,N));
+%             PredictClose(:,j1+1)=TMP(1:NumberOfAllPoint-1);
+%             j1=j1+2;
+        end
+        
+        
+        
+        Actual=OriginalData(33:length(OriginalData),Target);
+        Forecast=PredictClose(:,Target);
+        [Profit,SellAndBuy]=PaperStrategy(Actual,Forecast);
+        
+        Profit_tmp(:,Trial)=Profit;
+    end
+    ProfitTable=ProfitTable+Profit_tmp;
+    
 end
+save('ProfitTable','ProfitTable','SellAndBuy','NumberOfProfitTarget','NumberOfTrial','LoadName');
+clear
+load('ProfitTable','NumberOfProfitTarget','NumberOfTrial','LoadName');
+%% 1 iteration
+LoadName=['1' LoadName];
+one_iteration_ProfitTable=0;
 
-% ProfitTable=[PaperStrategy(LoadName,Target,NumberOfTrial) PaperStrategy(LoadName1,Target,NumberOfTrial)];
+for Target=1:NumberOfProfitTarget
+for Trial=1:NumberOfTrial
+        load([LoadName int2str(Trial)])
+        for N=1:NumberOfOUTPUT
+            FinalyHead(:,N)=[ABCOgBest.yHead(:,N) ;testyHead(:,N)];
+        end
+        TMP=[];
+        j1=1;
+        for N=1:NumberOfOUTPUT
+            TMP=OriginalData(31:length(OriginalData)-1,j1)+real(FinalyHead(:,N));
+            PredictClose(:,j1)=TMP(1:NumberOfAllPoint-1);
+%             TMP=OriginalData(31:length(OriginalData)-1,j1+1)+imag(FinalyHead(:,N));
+%             PredictClose(:,j1+1)=TMP(1:NumberOfAllPoint-1);
+%             j1=j1+2;
+        end
+        
+        
+        
+        Actual=OriginalData(33:length(OriginalData),Target);
+        Forecast=PredictClose(:,Target);
+        [Profit,SellAndBuy]=PaperStrategy(Actual,Forecast);
+        
+        Profit_tmp(:,Trial)=Profit;
+        
+end
+        one_iteration_ProfitTable=one_iteration_ProfitTable+Profit_tmp;
+end
+save('one_iteration_ProfitTable','one_iteration_ProfitTable','SellAndBuy');
+clear
 
-% Mean(StrategyAlpha+1)=mean(ProfitTable(:,1));
-% Mean(StrategyAlpha+1)=mean(ProfitTable(:,4));
+%% mean
+load('ProfitTable')
+load('one_iteration_ProfitTable')
+for i=1:300
+        Mean(i,1)=mean(ProfitTable(i,:));
+        Std(i,1)=std(ProfitTable(i,:));
+        Mean(i,2)=mean(one_iteration_ProfitTable(i,:));
+        Std(i,2)=std(one_iteration_ProfitTable(i,:));
+end
+max(Mean(:,1))
+max(Mean(:,2))
